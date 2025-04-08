1fa864bdb0730981ea8e45cfff10a0f793510590
m5242 <kruzhenevskiy@gmail.com>
2025-04-08 12:22:55 +0300

    planfix and mikrotik added

---
+++ mikrotik.py
@@ -0,0 +1,25 @@
+import paramiko
+
+
+def connect_via_ssh(host_ip, username, password, port=22):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host_ip, port=port, username=username, password=password)
+        return ssh
+    except Exception as e:
+        print(f"Ошибка подключения: {e}")
+        return None
+
+
+def execute_command(ssh, command):
+    try:
+        stdin, stdout, stderr = ssh.exec_command(command)
+        output = stdout.read().decode()
+        error = stderr.read().decode()
+        if error:
+            print(f"Ошибка: {error}")
+        return output
+    except Exception as e:
+        print(f"Ошибка при выполнении команды: {e}")
+        return None
