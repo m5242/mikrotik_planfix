f56bacd3aaa85871106fd08d80927952e7fc3f08
m5242 <kruzhenevskiy@gmail.com>
2025-04-10 10:54:17 +0300

    only mikrotik command from pf is shown

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ from mikrotik import *
 
 
 def main():
-    host_ip = settings.host_ip
-    username = settings.username
-    password = settings.password
-    command = input("type command: ")
-
-    ssh = connect_via_ssh(host_ip, username, password)
-    if ssh:
-        output = execute_command(ssh, command)
-        if output is not None:
-            print("response: ")
-            print(output.strip() if output else "")
-        ssh.close()
-    else:
-        print("Не удалось подключиться")
+    test_api()
+    # host_ip = settings.host_ip
+    # username = settings.username
+    # password = settings.password
+    # planfix_token = settings.planfix_token
+    # planfix_username = settings.planfix_username
+    # command = input("type command: ")
+    #
+    # ssh = connect_via_ssh(host_ip, username, password)
+    # if ssh:
+    #     output = execute_command(ssh, command)
+    #     if output is not None:
+    #         print("response: ")
+    #         print(output.strip() if output else "")
+    #     ssh.close()
+    # else:
+    #     print("Не удалось подключиться")
 
 
 if __name__ == "__main__":
--- planfix.py
+++ planfix.py
@@ -1,2 +1,33 @@
 import requests
 
+
+def test_api():
+    url = "https://engineering.planfix.ru/rest/report/26000/save/5388/data?chunk=0"
+    # url = "https://engineering.planfix.ru/rest/report/26000/generate"
+    # url = "https://engineering.planfix.ru/rest/report/26000/generate"
+    headers = {
+        "Authorization": "Bearer 92887b05010d215da04742e2031491fd",
+        "Accept": "application/json"
+    }
+
+    response = requests.post(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        header_row = next(row for row in data["data"]["rows"] if row["type"] == "Header")
+        column_index = next(
+            index for index, item in enumerate(header_row["items"])
+            if item["text"] == "Команда для маршрутизатора"
+        )
+
+        commands = []
+        for row in data["data"]["rows"]:
+            if row["type"] != "Header":
+                command = row["items"][column_index]["text"]
+                commands.append(command)
+                print(command)
+
+    else:
+        print(f"Ошибка: {response.status_code}")
+        print(response.text)
