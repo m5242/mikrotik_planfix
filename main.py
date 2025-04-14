bc6af8394600cf26770828e539f61585b9c23092
m5242 <kruzhenevskiy@gmail.com>
2025-04-14 17:41:47 +0300

    planfix to mikrotik (without changing status)

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
+import time
 import local_settings as settings
 from planfix import *
 from mikrotik import *
 
 
 def main():
-    test_api()
-    # host_ip = settings.host_ip
-    # username = settings.username
-    # password = settings.password
     # planfix_token = settings.planfix_token
     # planfix_username = settings.planfix_username
-    # command = input("type command: ")
-    #
-    # ssh = connect_via_ssh(host_ip, username, password)
-    # if ssh:
-    #     output = execute_command(ssh, command)
-    #     if output is not None:
-    #         print("response: ")
-    #         print(output.strip() if output else "")
-    #     ssh.close()
-    # else:
-    #     print("Не удалось подключиться")
+    # id = "5448"
+    # report_get_data_by_id(id)
+
+    host_ip = settings.host_ip
+    username = settings.username
+    password = settings.password
+
+    req_id = generate_report()
+    time.sleep(10)
+
+    status_id = report_get_by_req_id(req_id)
+    time.sleep(1)
+    commands = report_get_data_by_id(status_id)
+    print(commands)
+    time.sleep(1)
+    command_final = "\n".join(commands)
+
+    ssh = connect_via_ssh(host_ip, username, password)
+    if ssh:
+        output = execute_command(ssh, command_final)
+        if output is not None:
+            print("response: ")
+            print(output.strip() if output else "")
+        ssh.close()
+    else:
+        print("Не удалось подключиться")
 
 
 if __name__ == "__main__":
--- planfix.py
+++ planfix.py
@@ -1,12 +1,29 @@
 import requests
+import local_settings as settings
+
+planfix_token = settings.planfix_token
 
 
-def test_api():
-    url = "https://engineering.planfix.ru/rest/report/26000/save/5388/data?chunk=0"
-    # url = "https://engineering.planfix.ru/rest/report/26000/generate"
-    # url = "https://engineering.planfix.ru/rest/report/26000/generate"
+def report_get_by_req_id(req_id):
+    url = f"https://engineering.planfix.ru/rest/report/status/{str(req_id)}/"
     headers = {
-        "Authorization": "Bearer 92887b05010d215da04742e2031491fd",
+        "Authorization": f"Bearer {planfix_token}",
+        "Accept": "application/json"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        print(data["save"]["id"])
+        return data["save"]["id"]
+    else:
+        print(f"Код: {response.status_code}")
+        print(response.text)
+
+
+def report_get_data_by_id(id):
+    url = f"https://engineering.planfix.ru/rest/report/26000/save/{str(id)}/data?chunk=0"
+    headers = {
+        "Authorization": f"Bearer {planfix_token}",
         "Accept": "application/json"
     }
 
@@ -15,19 +32,37 @@ def test_api():
     if response.status_code == 200:
         data = response.json()
 
-        header_row = next(row for row in data["data"]["rows"] if row["type"] == "Header")
-        column_index = next(
-            index for index, item in enumerate(header_row["items"])
-            if item["text"] == "Команда для маршрутизатора"
-        )
 
-        commands = []
-        for row in data["data"]["rows"]:
-            if row["type"] != "Header":
-                command = row["items"][column_index]["text"]
-                commands.append(command)
-                print(command)
+        commands = [
+            row["items"][5]["text"]
+            for row in data["data"]["rows"]
+                if row["type"] == "Normal"
+        ]
+        return commands
 
     else:
         print(f"Ошибка: {response.status_code}")
         print(response.text)
+
+
+def generate_report():
+    try:
+        generate_url = "https://engineering.planfix.ru/rest/report/26000/generate"
+        headers = {
+            "Authorization": f"Bearer {planfix_token}",
+            "Accept": "application/json"
+        }
+
+        response = requests.post(generate_url, headers=headers)
+        response.raise_for_status()
+
+        generate_data = response.json()
+
+        req_id = generate_data["requestId"]
+        print(f"Отчёт успешно создан. ID запроса: {req_id}")
+        return req_id
+
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при генерации отчёта: {str(e)}")
+        return (False, None)
+
