5836d4349753e32648daa3e6ff5c764c967f6cab
m5242 <kruzhenevskiy@gmail.com>
2025-04-21 10:07:27 +0300

    version 1.01 (some bugs fixed)

--- main.py
+++ main.py
@@ -15,65 +15,71 @@ status_id_fin = settings.status_id_fin
 
 
 def granting_access():
-    print(f"Запуск granting_access в {time.strftime('%Y-%m-%d %H:%M:%S')}")
-    req_id = generate_report(granting_access_report_id)
-    time.sleep(10)
+    try:
+        print(f"Запуск granting_access в {time.strftime('%Y-%m-%d %H:%M:%S')}")
+        req_id = generate_report(granting_access_report_id)
+        time.sleep(10)
 
-    status_id = report_get_by_req_id(req_id)
-    time.sleep(1)
+        status_id = report_get_by_req_id(req_id)
+        time.sleep(1)
 
-    commands = report_get_data_by_id(status_id, granting_access_report_id)[0]
-    list_id_of_task = report_get_data_by_id(status_id, granting_access_report_id)[1]
+        commands = report_get_data_by_id(status_id, granting_access_report_id)[0]
+        list_id_of_task = report_get_data_by_id(status_id, granting_access_report_id)[1]
 
-    time.sleep(1)
-    command_final = "\n".join(commands)
+        time.sleep(1)
+        command_final = "\n".join(commands)
 
-    ssh = connect_via_ssh(host_ip, username, password)
-    if ssh:
-        output = execute_command(ssh, command_final)
-        if output is not None:
-            print("response from router: ")
-            print(output.strip() if output else "")
-        ssh.close()
-    else:
-        print("Не удалось подключиться")
+        ssh = connect_via_ssh(host_ip, username, password)
+        if ssh:
+            output = execute_command(ssh, command_final)
+            if output is not None:
+                print("response from router: ")
+                print(output.strip() if output else "")
+            ssh.close()
+        else:
+            print("Не удалось подключиться")
 
-    time.sleep(1)
-    for ids in list_id_of_task:
-        print(ids)
-        status_changing(ids, status_id_dp)
-    print("Скрипт granting_access отработал.\n\n")
+        time.sleep(1)
+        for ids in list_id_of_task:
+            print(ids)
+            status_changing(ids, status_id_dp)
+        print("Скрипт granting_access отработал.\n\n")
+    except Exception as e:
+        print(f"Ошибка при выполнении скрипта: {e}\n\n")
 
 
 def terminating_access():
-    print(f"Запуск terminating_access в {time.strftime('%Y-%m-%d %H:%M:%S')}")
-    req_id = generate_report(terminating_access_report_id)
-    time.sleep(10)
+    try:
+        print(f"Запуск terminating_access в {time.strftime('%Y-%m-%d %H:%M:%S')}")
+        req_id = generate_report(terminating_access_report_id)
+        time.sleep(10)
 
-    status_id = report_get_by_req_id(req_id)
-    time.sleep(1)
+        status_id = report_get_by_req_id(req_id)
+        time.sleep(1)
 
-    commands = report_get_data_by_id_for_term(status_id, terminating_access_report_id)[0]
-    list_id_of_task = report_get_data_by_id_for_term(status_id, terminating_access_report_id)[1]
+        commands = report_get_data_by_id_for_term(status_id, terminating_access_report_id)[0]
+        list_id_of_task = report_get_data_by_id_for_term(status_id, terminating_access_report_id)[1]
 
-    time.sleep(1)
-    command_final = "\n".join(commands)
+        time.sleep(1)
+        command_final = "\n".join(commands)
 
-    ssh = connect_via_ssh(host_ip, username, password)
-    if ssh:
-        output = execute_command(ssh, command_final)
-        if output is not None:
-            print("response from router: ")
-            print(output.strip() if output else "")
-        ssh.close()
-    else:
-        print("Не удалось подключиться")
+        ssh = connect_via_ssh(host_ip, username, password)
+        if ssh:
+            output = execute_command(ssh, command_final)
+            if output is not None:
+                print("response from router: ")
+                print(output.strip() if output else "")
+            ssh.close()
+        else:
+            print("Не удалось подключиться")
 
-    time.sleep(1)
-    for ids in list_id_of_task:
-        print(ids)
-        status_changing(ids, status_id_fin)
-    print("Скрипт terminating_access отработал.\n\n")
+        time.sleep(1)
+        for ids in list_id_of_task:
+            print(ids)
+            status_changing(ids, status_id_fin)
+        print("Скрипт terminating_access отработал.\n\n")
+    except Exception as e:
+        print(f"Ошибка при выполнении скрипта: {e}\n\n")
 
 
 schedule.every(1).hours.do(granting_access)
